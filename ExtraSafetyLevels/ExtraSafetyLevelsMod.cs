feccdde3b64c60604e5fe2e823997a88f8d7aa97
MajoraDev <Majora@Dev>
2022-05-30 19:24:00 +0200

    Add project files.

---
+++ ExtraSafetyLevels/ExtraSafetyLevelsMod.cs
@@ -0,0 +1,39 @@
+﻿using ExtraSafetyLevels;
+using MelonLoader;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC;
+
+[assembly: MelonInfo(typeof(ExtraSafetyLevelsMod), "Extra Safety Levels", "1.0.0", "Majora")]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonColor(ConsoleColor.Green)]
+
+
+namespace ExtraSafetyLevels
+{
+    public class ExtraSafetyLevelsMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(Initialize());
+        }
+        internal static IEnumerator Initialize()
+        {
+            while (GameObject.Find("UserInterface/MenuContent/Screens/Settings_Safety/_Buttons_UserLevel/") == null) yield return null;
+            var horizontalLayoutGroup = GameObject.Find("UserInterface/MenuContent/Screens/Settings_Safety/_Buttons_UserLevel").GetComponent<HorizontalLayoutGroup>();
+            horizontalLayoutGroup.childControlWidth = true;
+            horizontalLayoutGroup.childForceExpandWidth = true;
+            new SafetyLevel("Nuisance", Color.red, UserSocialClass.NegativeTrustLevel1, "<i>Nuisances</i> are confirmed trolls that have been reported/blocked/muted a lot. <i>Nuisances</i> should have the least amount of avatar features visible.").gameObject.transform.SetAsFirstSibling();
+            new SafetyLevel("LocalPlayer", Color.cyan, UserSocialClass.LocalPlayer, "You can apply safety on your Local-Player (Your own player).");
+            yield return null;
+        }
+    }
+}
+
+
---
+++ ExtraSafetyLevels/SafetyLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace ExtraSafetyLevels
+{
+    internal class SafetyLevel
+    {
+        public GameObject gameObject;
+        public SafetyLevel(string safetyName, UnityEngine.Color color, VRC.UserSocialClass socialClass, string description)
+        {
+            gameObject = GameObject.Instantiate(GameObject.Find("UserInterface/MenuContent/Screens/Settings_Safety/_Buttons_UserLevel/Button_Visitor"), GameObject.Find("UserInterface/MenuContent/Screens/Settings_Safety/_Buttons_UserLevel").transform);
+            gameObject.name = $"Button_{safetyName}";
+            var image = gameObject.GetComponent<UnityEngine.UI.Image>();
+            image.color = color;
+            var text = gameObject.GetComponentInChildren<UnityEngine.UI.Text>();
+            text.text = safetyName;
+            var rankToggle = gameObject.GetComponent<UiSafetyRankToggle>();
+            rankToggle.field_Public_UserSocialClass_0 = socialClass;
+            rankToggle.field_Public_String_0 = description;
+        }
+    }
+}
